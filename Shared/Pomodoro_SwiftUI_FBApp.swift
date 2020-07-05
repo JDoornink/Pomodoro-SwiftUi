ba0f364e25fb68dbe6d12602c8e19aa3a537a517
Josef <jdoorarg@gmail.com>
2020-07-05 11:11:07 -0700

    Initial Commit

---
+++ Shared/Pomodoro_SwiftUI_FBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_SwiftUI_FBApp.swift
+//  Shared
+//
+//  Created by JD on 7/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_SwiftUI_FBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
